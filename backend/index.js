0600c38b8af473417f77fe44959044a1a6cf461d
tibedeo <fayeza.tibedeo@gmail.com>
2025-12-03 22:51:21 +0100

    GraphQL

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log("Docker backend is running.");
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Bienvenue dans Galapagos GraphQL !",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 4000, host: '0.0.0.0' }).then(({ url }) => {
+  console.log(`GraphQL prêt à ${url}`);
+});
